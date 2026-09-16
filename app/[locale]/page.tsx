74f43dfb6f6ca5aabf24e3c74c697e0e2fa10aa1
Mohamed_Sokara <mosokara2007@gmail.com>
2026-09-16 18:13:57 +0300

    feat: add developer introduction (about) section

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,9 +1,11 @@
-import Hero from "@/components/layout/hero";
+import About from "@/components/sections/about";
+import Hero from "@/components/sections/hero";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <About />
     </main>
   );
 }
